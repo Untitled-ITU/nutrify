ebedc8294ec1cd11b3a4cc70b05de4480f18c3f7
keremnuman <cakirkeremnuman@gmail.com>
2025-11-12 22:40:02 +0300

    feat(profile): GET for user profile.

--- backend/app/auth/routes.py
+++ backend/app/auth/routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request, jsonify
-from flask_jwt_extended import create_access_token, get_jwt, jwt_required
+from flask_jwt_extended import create_access_token, get_jwt, jwt_required, get_jwt_identity
 
 from ...extensions import jwt,db
 from .models import User
@@ -61,3 +61,16 @@ def chef_only_data():
         return jsonify(message="Hello Chef!")
     else:
         return jsonify(message="You don't have permission to view this area!"), 403
+
+@auth_bp.route('/profile', methods=['GET'])
+@jwt_required()
+def get_profile():
+    current_user_email = get_jwt_identity()
+    user = User.query.filter_by(email=current_user_email).first_or_404()
+    
+    return jsonify(
+        id=user.id,
+        email=user.email,
+        name=user.name,
+        role=user.role
+    ), 200
