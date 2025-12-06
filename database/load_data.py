2771f296daa8e892cb756f74cf691700ea395f2b
Mete Yılmam <115945927+GalanKral@users.noreply.github.com>
2025-12-06 15:20:54 +0300

    refactor: simplify data loading

--- database/load_data.py
+++ database/load_data.py
@@ -1,43 +1,17 @@
+import os
+
 import pandas as pd
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 
-DB_PASSWORD = 'your_password' #enter your real password inside
 
-CSV_FILENAME = 'recipes.csv' 
+load_dotenv("../backend/.env")
 
-print("1. Reading the CSV file... (This might take a moment)")
-try:
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+CSV_FILENAME = 'recipes.csv'
 
-    df = pd.read_csv(CSV_FILENAME)
-    
-    df_db = df.rename(columns={
-        'recipe_title': 'recipe_title',
-        'category': 'category',
-        'subcategory': 'subcategory',
-        'description': 'description',
-        'directions': 'directions',
-        'num_ingredients': 'num_ingredients',
-        'num_steps': 'num_steps',
-        'ingredients': 'original_ingredients'
-    })
+df = pd.read_csv(CSV_FILENAME)
+df_db = df.rename(columns={'ingredients': 'original_ingredients'})
 
-    columns_to_keep = [
-        'recipe_title', 'category', 'subcategory', 'description', 
-        'directions', 'num_ingredients', 'num_steps', 'original_ingredients'
-    ]
-    df_final = df_db[columns_to_keep]
-
-    print("2. Connecting to the Database...")
-
-    engine = create_engine('postgresql://postgres:12345@127.0.0.1:5433/nutrify_db')
-
-    print("3. Uploading data to PostgreSQL... (This may take 1-2 minutes)")
-    
-    df_final.to_sql('recipe', engine, if_exists='append', index=False)
-
-    print("✅ SUCCESS! All recipes have been loaded into the database.")
-
-except Exception as e:
-    print(f"❌ AN ERROR OCCURRED:\n{e}")
-
-input("\nPress Enter to exit...")
+engine = create_engine(f'postgresql://postgres:{DB_PASSWORD}@127.0.0.1:5433/nutrify_db')
+df_db.to_sql('recipe', engine, if_exists='replace', index=False)
