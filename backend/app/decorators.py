a79c78375a3a60ec97eec9e1ee748a14ea0daef7
Mete Yılmam <115945927+GalanKral@users.noreply.github.com>
2025-12-11 00:34:48 +0300

    feat: add chef_required and login_required decorators

--- backend/app/decorators.py
+++ backend/app/decorators.py
@@ -19,3 +19,26 @@ def admin_required(fn):
         else:
             return jsonify(msg="Admins only! You don't have permission."), 403
     return decorator
+
+
+def chef_required(fn):
+    @wraps(fn)
+    def decorator(*args, **kwargs):
+        verify_jwt_in_request()
+
+        current_user_email = get_jwt_identity()
+        user = User.query.filter_by(email=current_user_email).first()
+
+        if user and user.role in ['chef', 'admin']:
+            return fn(*args, **kwargs)
+        else:
+            return jsonify(msg="Chef or Admin access required."), 403
+    return decorator
+
+
+def login_required(fn):
+    @wraps(fn)
+    def decorator(*args, **kwargs):
+        verify_jwt_in_request()
+        return fn(*args, **kwargs)
+    return decorator
